b3dbee7a90d4809298d0c894174ebf98e4fa5d40
Jamie <jamie_mcd23@hotmail.com>
2013-04-26 01:05:07 +0100

    first commit

--- countdown.cpp
+++ countdown.cpp
@@ -1,4 +1,4 @@
-//Last update 10/2/13
+//Last update 5/4/13
 
 #include <iostream>
 #include <map>
@@ -9,6 +9,7 @@
 
 using namespace std;
 
+//returns a sorted string input
 string getInput(){
 	string userInput;
 	cout << "Enter an arrangement of letters" << endl;
@@ -17,6 +18,7 @@ string getInput(){
 	return userInput;
 }
 
+//Finds keys which match sorted input, outputs all values for this key 
 void findAnagram(map<string, vector<string> > stringMap, string userInput ){
 	map<string, vector<string> >::const_iterator it;
 
@@ -46,12 +48,32 @@ string displayEverything(){
 	}*/
 }
 
+//Write comments on this 
+void permute(string str, int d){
+	if(d==str.length()){
+		sort(str.begin(),str.end());
+		cout << str << endl;
+	}else{
+		char swap;
+		char lastSwap;
+		for(int i=d; i<=str.length();i++){
+			if(lastSwap!=str[i]){
+				lastSwap=str[i];
+			}
+			swap = str[d];
+			str[d] = str[i];
+			str[i] = swap;
+			permute(str,d+1);
+		}
+	}
+}
+
+
+
 int main(){
 	map<string, vector<string> > stringMap;
 	map<string, vector<string> >::const_iterator it;
-
-	int largest = 0;
-	string largestword;
+	//vector<string>
 	string line;
 	string lineOriginal;
 	string userInput;
@@ -68,6 +90,7 @@ int main(){
 
 			//Adding first element if none exist
 			if(stringMap.size()==0){
+				//addVector is blank here, doesn't have first value added to it.
 				stringMap.insert(std::pair< string,vector<string> > (line,addVector));
 			}
 			else{ 
@@ -75,10 +98,10 @@ int main(){
 				if(stringMap.find(line) == stringMap.end()){
 					addVector.push_back(lineOriginal);
 					stringMap.insert(std::pair< string,vector<string> > (line,addVector));
-					addVector.clear();	
+				addVector.clear();	
 				}else{
 					stringMap[line].push_back(lineOriginal);
-				}
+			}
 			}
 		}
 		myfile.close(); 
@@ -88,9 +111,24 @@ int main(){
 		return 1;
 	}
 
-	userInput = getInput
+	userInput = getInput();
 
-	//Outputs any matching key and its value
+	permute(userInput, 0);
+	
+	map<string, vector<string> >::const_iterator iterSearch;
+	iterSearch = stringMap.find(userInput); 
+
+	//Does it exist? Yes - Output, No - Say no to if
+	if(iterSearch == stringMap.end()){
+		cout << "There is no word found";
+	}else{
+		cout << iterSearch->first << " - ";
+		vector<string> tempVec = (*iterSearch).second;	
+		
+		for(int i=0;i<tempVec.size(); i++){	
+			cout << tempVec[i] << ", ";
+		}
+	}
 
 	return 0;
 } 
