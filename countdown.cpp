0ced6806533f68e3e4dfcd9541413e25764e3b2c
jamie23 <jamie_mcd23@hotmail.com>
2015-04-01 20:23:37 +0100

    spaces instead of tabs

--- countdown.cpp
+++ countdown.cpp
@@ -39,33 +39,10 @@ string displayEverything(){
 	}*/
 }
 
-//Think this gives every permutation of word
-void permute(string str, int d){
-	if(d==str.length()){
-		sort(str.begin(),str.end());
-		cout << str << endl;
-	}else{
-		char swap;
-		char lastSwap;
-		for(int i=d; i<=str.length();i++){
-			if(lastSwap!=str[i]){
-				lastSwap=str[i];
-			}
-			swap = str[d];
-			str[d] = str[i];
-			str[i] = swap;
-			permute(str,d+1);
-		}
-	}
-}
-
-
-
 string rtnSortedInput(string userInput){
 	sort(userInput.begin(), userInput.end());
 	return userInput;
 }
-
 
 map<string, vector<string> > generateMap(){
 	map<string, vector<string> > stringMap;
@@ -108,6 +85,23 @@ map<string, vector<string> > generateMap(){
 	return stringMap;
 }
 
+void checkWord(&map<string, vector<string> > stringMap, string sortedInput){
+	map<string, vector<string> >::const_iterator iterSearch;
+	iterSearch = stringMap.find(sortedInput); 
+
+	//Does key exist
+	if(iterSearch == stringMap.end()){
+		cout << "There is no word found";
+	}else{
+		cout << iterSearch->first << " - ";
+		vector<string> tempVec = (*iterSearch).second;	
+		
+		for(int i=0;i<tempVec.size(); i++){	
+			cout << tempVec[i] << ", ";
+		}
+	}
+}
+
 int main(){
 	map<string, vector<string> > stringMap;
 	map<string, vector<string> >::const_iterator it;
@@ -120,25 +114,7 @@ int main(){
 
 	stringMap = generateMap();
 
-	//Finds every permutation of a word passed into it
-	permute(sortedInput, 0);
-	
-	map<string, vector<string> >::const_iterator iterSearch;
-	iterSearch = stringMap.find(sortedInput); 
-
-	//Does key exist
-	/*
-	if(iterSearch == stringMap.end()){
-		cout << "There is no word found";
-	}else{
-		cout << iterSearch->first << " - ";
-		vector<string> tempVec = (*iterSearch).second;	
-		
-		for(int i=0;i<tempVec.size(); i++){	
-			cout << tempVec[i] << ", ";
-		}
-	}
-	*/
+	checkWord(*stringMap,sortedInpu);
 
 	return 0;
 } 
