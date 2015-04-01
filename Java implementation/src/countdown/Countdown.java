e57f4197780596ee97ea306a64c138b8a11db4f1
jamie23 <jamie_mcd23@hotmail.com>
2015-04-01 20:34:46 +0100

    Vim indenting

--- Java implementation/src/countdown/Countdown.java
+++ Java implementation/src/countdown/Countdown.java
@@ -1,23 +1,23 @@
 package countdown;
 
 public class Countdown {
-		
+
 	private static int[] initialiseArray(int num){
-        int[] positions = new int[num]; 
-        
-        for(int i = 0;i<num;i++){
-        	positions[i] = i;
-        }
-        return positions;
+		int[] positions = new int[num]; 
+
+		for(int i = 0;i<num;i++){
+			positions[i] = i;
+		}
+		return positions;
 	}
 
 	public static void main(String[] args){
 		Dictionary dictionary = new Dictionary();
 		String word = dictionary.getSortedInput();
-		
+
 		for(int i = 4;i<=9;i++){
-                 int[] positions = initialiseArray(i);
-                 dictionary.findWord(positions,word);
+			int[] positions = initialiseArray(i);
+			dictionary.findWord(positions,word);
 		}
 	}
 }
